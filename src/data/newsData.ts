0760c0c88e7beff51b7e5d2f70dba7d8e4138d37
florenceNamukisa <florencenamukisa08@gmail.com>
2026-02-03 12:39:36 +0300

    adjusted ads section

--- src/data/newsData.ts
+++ src/data/newsData.ts
@@ -1,9 +1,10 @@
 import { Post, Ad, Short, Video } from "../types";
 
 export const ads: Ad[] = [
-  { id: "a1", image: "https://picsum.photos/seed/ad1/1200/100", title: "Jumia Black Friday", link: "#" },
-  { id: "a2", image: "https://picsum.photos/seed/ad2/300/250", title: "Betway Winner", link: "#" },
-  { id: "a3", image: "https://picsum.photos/seed/ad3/300/600", title: "Travel Start", link: "#" }
+  { id: "a1", image: "/ads/nbk-premier-billboard.svg", title: "NBK Premier Solutions Ltd", link: "https://www.nbkpremier.com" },
+  { id: "a2", image: "/legacy_pearls_ad.jpg", title: "Legacy Pearls", link: "#" },
+  { id: "a3", image: "/ads/nbk-premier-300x600.svg", title: "NBK Premier Solutions", link: "https://www.nbkpremier.com" },
+  { id: "a4", image: "/ads/nbk-premier-300x250.svg", title: "NBK Premier Services", link: "https://www.nbkpremier.com" }
 ];
 
 export const shorts: Short[] = [
